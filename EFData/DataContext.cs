447858ed7d58544881faa0a9253538336e37f2ed
evgeny174 <vostrik174@gmail.com>
2021-03-22 23:15:00 +0500

    init

---
+++ EFData/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFData
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
